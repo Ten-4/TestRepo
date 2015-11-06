b341bd1fba0554825ffa508c35b46a958c490d35
Tommi Pesu <tommi.pesu13@imperial.ac.uk>
2015-11-06 22:34:22 +0000

    tests used to help with test regexping

--- arbitrage.py
+++ arbitrage.py
@@ -2,14 +2,13 @@
 class Arbitrage_Engine(object):
 	
 	def __init__(self):
-		raise NotImplemented
-		
+		raise NotImplementedError
+
 	def push_orders(self, orders):
-		#order is of the form ["exchange:stock:price:buy/sell_sticker",...]
-		raise NotImplemented
+		#order is of the form ["ts:exchange:stock:price:buy/sell_sticker",...]
+		raise NotImplementedError
 		
 	def execute(self):
-		arbs = []
 		#Finds all arbitrage opportunities in the given set of orders.
-		raise NotImplemented
-		return arbs
+		#of the form [ (ts, buy_order, sell_order)...]
+		raise NotImplementedError
--- test_arbitrage.py
+++ test_arbitrage.py
@@ -1,10 +1,27 @@
+from arbitrage import Arbitrage_Engine
 
-def test_main():
-	input = "1 2"
-	L = [int(i) for i in input.split(" ")]	
-	res = sum(L)
-	assert(res == 3)
+def test_init():
+	AE = Arbitrage_Engine()
 
+def test_simple_arbitrage():
+	AE = Arbitrage_Engine()
+	orders = ["1:NYSE:AAPL:220.00:S","2:NASDAQ:AAPL:220.20:B"]
+	AE.push_orders(orders)
+	res = AE.execute()
+	ans = [(2,"2:NASDAQ:AAPL:220.20:B","1:NYSE:AAPL:220.00:S")]
+	assert ans == res
 
+def test_A():
+	pass
 
+def test_B():
+	pass
+
+def test_C():
+	pass
+
+def test_D():
+	pass
+	
+	
 #/homes/ttp09/.local/lib/python2.7/site-packages
